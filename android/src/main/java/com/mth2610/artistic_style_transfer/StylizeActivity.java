3c26501575ba9d8f213020facdf54908bbac6914
MacOS <macos@MacOSs-MacBook-Pro.local>
2019-05-03 14:06:16 +0700

    increase memory management

--- android/src/main/java/com/mth2610/artistic_style_transfer/StylizeActivity.java
+++ android/src/main/java/com/mth2610/artistic_style_transfer/StylizeActivity.java
@@ -29,41 +29,40 @@ public class StylizeActivity {
     TensorFlowInferenceInterface tensorFlowInferenceInterface;
     Bitmap inputBitmap;
     Bitmap croppedBitmap;
-    Bitmap scacledBitmap;
+//    Bitmap scacledBitmap;
 
     public StylizeActivity(Context context) {
         tensorFlowInferenceInterface = new TensorFlowInferenceInterface(context.getAssets(),MODEL_FILE);
     }
 
-
     public String styleTransfer(Context context, final Integer[] styles, String inputFilePath, final String outputDir, final int quality, float styleFactor, boolean convertToGrey) {
 //        int desiredSize = 256;
         //tensorFlowInferenceInterface = new TensorFlowInferenceInterface(context.getAssets(),MODEL_FILE);
+        Log.i("Availabel memory 1", String.valueOf(getAvailabelMemory()));
         String outputFilePath = null;
         inputBitmap = BitmapFactory.decodeFile(inputFilePath);
         int inputWidth = inputBitmap.getWidth();
         int inputHeight = inputBitmap.getHeight();
+        Log.i("Availabel memory 2", String.valueOf(getAvailabelMemory()));
+        croppedBitmap =  Bitmap.createScaledBitmap(inputBitmap, inputWidth*quality/100, inputHeight*quality/100, false);
+        Log.i("Availabel memory 3", String.valueOf(getAvailabelMemory()));
 
-        scacledBitmap =  Bitmap.createScaledBitmap(inputBitmap, inputWidth*quality/100, inputHeight*quality/100, false);
+        Log.i("Availabel memory 4", String.valueOf(getAvailabelMemory()));
 
-        Log.i("Availabel memory", String.valueOf(getAvailabelMemory()));
+        int desiredWidth = 128*(int)Math.floor(croppedBitmap.getWidth()/128);
+        int desiredHeight = 128*(int)Math.floor(croppedBitmap.getHeight()/128);
 
-        // free up inputBitmap
-       inputBitmap = null;
-
-        int desiredWidth = 128*(int)Math.floor(scacledBitmap.getWidth()/128);
-        int desiredHeight = 128*(int)Math.floor(scacledBitmap.getHeight()/128);
-
-        int previewWidth = scacledBitmap.getWidth();
-        int previewHeight = scacledBitmap.getHeight();
-
-        int desiredSize = Math.max(desiredWidth, desiredHeight);
-
+        int previewWidth = croppedBitmap.getWidth();
+        int previewHeight = croppedBitmap.getHeight();
+        int desiredSize = Math.min(desiredWidth, desiredHeight);
         // scale to square
-        croppedBitmap = Bitmap.createScaledBitmap(scacledBitmap, desiredSize, desiredSize, false);
+        croppedBitmap = Bitmap.createScaledBitmap(croppedBitmap, desiredSize, desiredSize, false);
+        Log.i("Availabel memory 5", String.valueOf(getAvailabelMemory()));
 
         // free up memory
-        scacledBitmap = null;
+//        croppedBitmap = croppedBitmap;
+
+        Log.i("Availabel memory 6", String.valueOf(getAvailabelMemory()));
 
         float[] styleVals = {
                 0.0f, 0.0f, 0.0f, 0.0f,
@@ -81,13 +80,17 @@ public class StylizeActivity {
             styleVals[styles[i]] = (float) 1.0*styleFactor/numStyles;
         }
 
+        System.gc();
+
         try{
-            if(getAvailabelMemory()<croppedBitmap.getWidth()*croppedBitmap.getHeight()*4.5){
+            Log.i("Availabel memory 7", String.valueOf(getAvailabelMemory()));
+            if(getAvailabelMemory()<croppedBitmap.getWidth()*croppedBitmap.getHeight()*5){
                 Log.i("memory", String.valueOf(getAvailabelMemory()));
                 throw new Exception("Out of memory");
             }
+            Log.i("Availabel memory 8", String.valueOf(getAvailabelMemory()));
 
-
+            // force garbage collector to run
             intValues = new int[croppedBitmap.getWidth()*croppedBitmap.getHeight()];
             floatValues = new float[croppedBitmap.getWidth()*croppedBitmap.getHeight()*3];
 
@@ -100,17 +103,17 @@ public class StylizeActivity {
                 floatValues[i * 3 + 2] = (val & 0xFF) / 255f;
             }
 
-            Log.i("floatValues", floatValues.toString());
-
+            Log.i("Availabel memory 9", String.valueOf(getAvailabelMemory()));
 
             tensorFlowInferenceInterface.feed(INPUT_NODE, floatValues, 1, croppedBitmap.getWidth(), croppedBitmap.getHeight(), 3);
             tensorFlowInferenceInterface.feed(STYLE_NODE, styleVals, NUM_STYLES);
             // Execute the output node's dependency sub-graph.
+
+            Log.i("Availabel memory 10 ***", String.valueOf(getAvailabelMemory()));
             tensorFlowInferenceInterface.run(new String[] {OUTPUT_NODE}, false);
             // Copy the data from TensorFlow back into our array
-            Log.i("outputTensor",tensorFlowInferenceInterface.getStatString());
-
             tensorFlowInferenceInterface.fetch(OUTPUT_NODE, floatValues);
+            Log.i("Availabel memory 11", String.valueOf(getAvailabelMemory()));
 
             for (int i = 0; i < intValues.length; ++i) {
                 intValues[i] =
@@ -120,6 +123,9 @@ public class StylizeActivity {
                                 | ((int) (floatValues[i * 3 + 2] * 255f));
             }
 
+            floatValues = null;
+
+            Log.i("Availabel memory 12", String.valueOf(getAvailabelMemory()));
 
             croppedBitmap.setPixels(intValues, 0, croppedBitmap.getWidth(), 0, 0, croppedBitmap.getWidth(), croppedBitmap.getHeight());
 
@@ -153,9 +159,15 @@ public class StylizeActivity {
                 outputFilePath = outputFile.getAbsolutePath();
 
                 // free up memory
+                croppedBitmap.recycle();
                 croppedBitmap = null;
+                inputBitmap.recycle();
+                inputBitmap = null;
                 intValues = null;
                 floatValues = null;
+
+                // force garbage collector to run
+                System.gc();
             } catch (Exception e) {
                 e.printStackTrace();
                 // free up memory
@@ -178,10 +190,84 @@ public class StylizeActivity {
     }
 
     void freeUpMemory(){
+        inputBitmap.recycle();
         inputBitmap = null;
-        scacledBitmap = null;
+        croppedBitmap.recycle();
         croppedBitmap = null;
         intValues = null;
         floatValues = null;
     }
 }
+
+
+//    int numRowSegments = desiredWidth/128;
+//    int numColSegments = desiredHeight/128;
+//
+//        for(int x = 0; x < numRowSegments; ++x) {
+//        for (int y = 0; y < numColSegments; ++y) {
+//        // 128*128 = 16384
+//        intValues = new int[16384];
+//        // 128*128*3 = 49152
+//        floatValues = new float[49152];
+//        croppedBitmap.getPixels(intValues, 0, 128,128*x, 128*y, 128, 128);
+//        for (int i = 0; i < intValues.length; ++i) {
+//final int val = intValues[i];
+//        floatValues[i * 3] = ((val >> 16) & 0xFF) / 255f;
+//        floatValues[i * 3 + 1] = ((val >> 8) & 0xFF) / 255f;
+//        floatValues[i * 3 + 2] = (val & 0xFF) / 255f;
+//        }
+//
+//        tensorFlowInferenceInterface.feed(INPUT_NODE, floatValues, 1, 128, 128, 3);
+//        tensorFlowInferenceInterface.feed(STYLE_NODE, styleVals, NUM_STYLES);
+//        tensorFlowInferenceInterface.run(new String[] {OUTPUT_NODE}, false);
+//        tensorFlowInferenceInterface.fetch(OUTPUT_NODE, floatValues);
+//
+//        for (int i = 0; i < intValues.length; ++i) {
+//        intValues[i] =
+//        0xFF000000
+//        | (((int) (floatValues[i * 3 ] * 255f)) << 16)
+//        | (((int) (floatValues[i * 3  + 1] * 255f)) << 8)
+//        | ((int) (floatValues[i * 3 + 2] * 255f));
+//        }
+//        croppedBitmap.setPixels(intValues, 0, 128,128*x, 128*y, 128, 128);
+//        }
+//        }
+//
+//        String outputFileName = String.valueOf(System.currentTimeMillis()) + ".jpeg";
+//        File outputFile = new File(outputDir +"/" + outputFileName);
+//        if (outputFile.exists()) outputFile.delete();
+//
+//        // scacle back
+//        croppedBitmap = Bitmap.createScaledBitmap(croppedBitmap, previewWidth, previewHeight, false);
+//
+//        if(convertToGrey==true) {
+//        Canvas canvas = new Canvas(croppedBitmap);
+//        Paint paint = new Paint();
+//        ColorMatrix cm = new ColorMatrix();
+//        cm.setSaturation(0);
+//        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+//        paint.setColorFilter(f);
+//        canvas.drawBitmap(croppedBitmap, 0, 0, paint);
+//        }
+//
+//        try {
+//        FileOutputStream out = new FileOutputStream(outputFile);
+//        croppedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+//        out.flush();
+//        out.close();
+//        ExifInterface inputExif = new ExifInterface(inputFilePath);
+//        ExifInterface outputExif = new ExifInterface(outputFile.getAbsolutePath());
+//        Log.i("inputOri", String.valueOf(inputExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)));
+//        outputExif.setAttribute(ExifInterface.TAG_ORIENTATION, inputExif.getAttribute(ExifInterface.TAG_ORIENTATION));
+//        outputExif.saveAttributes();
+//        outputFilePath = outputFile.getAbsolutePath();
+//
+//        // free up memory
+//        croppedBitmap = null;
+//        intValues = null;
+//        floatValues = null;
+//        } catch (Exception e) {
+//        e.printStackTrace();
+//        // free up memory
+//        freeUpMemory();
+//        }
